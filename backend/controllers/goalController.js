31fe10552e9f79ebfc7248e3e9aa5266b0253467
Simon Žižek <spymon12@gmail.com>
2022-02-21 12:14:32 +0100

    Changed typo in the name of the file

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,65 @@
+const asyncHandler = require('express-async-handler')
+const Goal = require('../models/goalModel')
+
+// @desc Set goal
+// @route POST /api/goals
+// @access Private
+const setGoal = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400)
+    throw new Error('please add a text field')
+  }
+
+  const goal = await Goal.create({
+    text: req.body.text,
+  })
+
+  res.status(200).json({ goal })
+})
+
+// @desc Get goals
+// @route GET /api/goals
+// @access Private
+const getGoals = asyncHandler(async (req, res) => {
+  const goals = await Goal.find()
+
+  res.status(200).send(goals)
+})
+
+// @desc Update goal
+// @route PUT /api/goals/:id
+// @access Private
+const updateGoal = asyncHandler(async (req, res) => {
+  const goal = await Goal.findById(req.params.id)
+  if (!goal) {
+    res.status(400)
+    throw new Error('Goal not found')
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  })
+
+  res.status(200).json(updatedGoal)
+})
+
+// @desc Delete goal
+// @route DELETE /api/goals/:id
+// @access Private
+const deleteGoal = asyncHandler(async (req, res) => {
+  const goal = await Goal.findById(req.params.id)
+  if (!goal) {
+    res.status(400)
+    throw new Error('Goal not found')
+  }
+  await goal.remove()
+
+  res.status(200).json({ id: req.params.id })
+})
+
+module.exports = {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+}
